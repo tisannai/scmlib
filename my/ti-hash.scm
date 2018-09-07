7b3a62032168717c51e1c4e1f6accd9e4aff2357
Tero Isannainen <tero.isannainen@gmail.com>
2018-09-07 13:46:37 +0300

    Hash lib with short names.

---
+++ my/ti-hash.scm
@@ -0,0 +1,28 @@
+;; -*-scheme-chicken-*-
+
+;; ti-hash module provides short names for common hash functions.
+
+(module ti-hash
+    (
+        h-new
+        h-set
+        h-get
+        h-del
+        h-each
+        h-keys
+        h-values
+        )
+
+    (import scheme)
+    (import chicken)
+
+    (use srfi-69)
+
+    (define h-new make-hash-table)
+    (define h-set hash-table-set!)
+    (define h-get hash-table-ref)
+    (define h-del hash-table-delete!)
+    (define h-each hash-table-for-each)
+    (define h-keys hash-table-keys)
+    (define h-vals hash-table-values)
+    )
